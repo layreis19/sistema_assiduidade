25fccad89c273f08629044f4277ca2f015a74bf9
DiogoPinto <diogopinto.estudos@gmail.com>
2026-09-14 16:09:42 +0100

    ficheiro .gitignore

--- assiduidade.py
+++ assiduidade.py
@@ -1,15 +1,18 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk, messagebox
 
-
+load_dotenv()  
 
 
 conn = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    password="reis2016",
-    database="ASSIDUIDADE"
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 
 cursor = conn.cursor()
