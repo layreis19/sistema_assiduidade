b4d0b195baae13ef850c228d6da27e905c1a9daf
layreis19 <layreis1999@gmail.com>
2026-09-14 15:51:04 +0100

    visual tkinter

--- assiduidade.py
+++ assiduidade.py
@@ -5,6 +5,7 @@ from tkinter import ttk, messagebox
 
 
 
+
 conn = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
@@ -15,3 +16,11 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 print("Ligação bem sucedida!")
+
+janela = tk.Tk()
+janela.title("Gestão de Assiduidade")
+janela.geometry("800x800")
+
+
+
+janela.mainloop()
